41c7858ebcb2038e01b0e4e24b2b5f9faaff2dae
Seoyoung Ahn <seoyoungahn@Seoyoungs-MacBook-Pro.local>
2023-04-12 13:50:21 -0500

    isolated adding chess pieces

--- app/src/main/java/Chess/view/ChessBoardPrototype.java
+++ app/src/main/java/Chess/view/ChessBoardPrototype.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 import Chess.model.ChessModel;
-import Chess.view.UnicodeMap;
+import Chess.view.ChessPieces;
 
 public class ChessBoardPrototype extends JFrame implements ActionListener {
     private JButton[][] boardSegment = new JButton[8][8];
@@ -60,82 +60,12 @@ public class ChessBoardPrototype extends JFrame implements ActionListener {
 
         mainPanel.add(boardPanel);
 
-        this.addPieces();
+        ChessPieces chessPieces = new ChessPieces(boardSegment);
 
         frame.add(mainPanel);
         frame.pack();
         frame.setVisible(true);
 
-    }
-
-    /**
-     * Helper function to add pieces to board.
-     */
-    private void addPieces() {
-        for (int i = 0; i < 8; i++) {
-            JLabel wPawnLabel = new JLabel(UnicodeMap.wPawn);
-            // Set font size to 32
-            wPawnLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-            wPawnLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-            boardSegment[6][i].add(wPawnLabel);
-
-            JLabel bPawnLabel = new JLabel(UnicodeMap.bPawn);
-            bPawnLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-            bPawnLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-            boardSegment[1][i].add(bPawnLabel);
-
-            if (i == 0 || i == 7) {
-                JLabel wRookLabel = new JLabel(UnicodeMap.wRook);
-                wRookLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                wRookLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[7][i].add(wRookLabel);
-
-                JLabel bRookLabel = new JLabel(UnicodeMap.bRook);
-                bRookLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                bRookLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[0][i].add(bRookLabel);
-            } else if (i == 1 || i == 6) {
-                JLabel wKnightLabel = new JLabel(UnicodeMap.wKnight);
-                wKnightLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                wKnightLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[7][i].add(wKnightLabel);
-
-                JLabel bKnightLabel = new JLabel(UnicodeMap.bKnight);
-                bKnightLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                bKnightLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[0][i].add(bKnightLabel);
-            } else if (i == 2 || i == 5) {
-                JLabel wBishopLabel = new JLabel(UnicodeMap.wBishop);
-                wBishopLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                wBishopLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[7][i].add(wBishopLabel);
-
-                JLabel bBishopLabel = new JLabel(UnicodeMap.bBishop);
-                bBishopLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                bBishopLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[0][i].add(bBishopLabel);
-            } else if (i == 3) {
-                JLabel wQueenLabel = new JLabel(UnicodeMap.wQueen);
-                wQueenLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                wQueenLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[7][i].add(wQueenLabel);
-
-                JLabel bQueenLabel = new JLabel(UnicodeMap.bQueen);
-                bQueenLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                bQueenLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[0][i].add(bQueenLabel);
-            } else if (i == 4) {
-                JLabel wKingLabel = new JLabel(UnicodeMap.wKing);
-                wKingLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                wKingLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[7][i].add(wKingLabel);
-
-                JLabel bKingLabel = new JLabel(UnicodeMap.bKing);
-                bKingLabel.setFont(new Font("Dialog", Font.BOLD, 32));
-                bKingLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-                boardSegment[0][i].add(bKingLabel);
-            }
-        }
     }
 
     private void updateGUI() {
---
+++ app/src/main/java/Chess/view/ChessPieces.java
@@ -0,0 +1,84 @@
+package Chess.view;
+
+import java.awt.*;
+import javax.swing.*;
+import Chess.view.UnicodeMap;
+
+public class ChessPieces 
+{
+    private JButton[][] boardSegment;
+
+    public ChessPieces(JButton[][] boardSegment)
+    {
+        this.boardSegment = boardSegment;
+        this.addPieces();
+    }
+
+    private void initializePiece(JLabel label)
+    {
+        label.setFont(new Font("Dialog", Font.BOLD, 45));
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+    }
+
+    /**
+     * Helper function to add pieces to board.
+     */
+    public void addPieces() {
+        for (int i = 0; i < 8; i++) {
+            JLabel wPawnLabel = new JLabel(UnicodeMap.wPawn);
+            initializePiece(wPawnLabel);
+            boardSegment[6][i].add(wPawnLabel);
+
+            JLabel bPawnLabel = new JLabel(UnicodeMap.bPawn);
+            initializePiece(bPawnLabel);
+            boardSegment[1][i].add(bPawnLabel);
+
+            if (i == 0 || i == 7) {
+                JLabel wRookLabel = new JLabel(UnicodeMap.wRook);
+                initializePiece(wRookLabel);
+                boardSegment[7][i].add(wRookLabel);
+
+                JLabel bRookLabel = new JLabel(UnicodeMap.bRook);
+                initializePiece(bRookLabel);
+                boardSegment[0][i].add(bRookLabel);
+
+            } else if (i == 1 || i == 6) {
+                JLabel wKnightLabel = new JLabel(UnicodeMap.wKnight);
+                initializePiece(wKnightLabel);
+                boardSegment[7][i].add(wKnightLabel);
+
+                JLabel bKnightLabel = new JLabel(UnicodeMap.bKnight);
+                initializePiece(bKnightLabel);
+                boardSegment[0][i].add(bKnightLabel);
+                
+            } else if (i == 2 || i == 5) {
+                JLabel wBishopLabel = new JLabel(UnicodeMap.wBishop);
+                initializePiece(wBishopLabel);
+                boardSegment[7][i].add(wBishopLabel);
+
+                JLabel bBishopLabel = new JLabel(UnicodeMap.bBishop);
+                initializePiece(bBishopLabel);
+                boardSegment[0][i].add(bBishopLabel);
+
+            } else if (i == 3) {
+                JLabel wQueenLabel = new JLabel(UnicodeMap.wQueen);
+                initializePiece(wQueenLabel);
+                boardSegment[7][i].add(wQueenLabel);
+
+                JLabel bQueenLabel = new JLabel(UnicodeMap.bQueen);
+                initializePiece(bQueenLabel);
+                boardSegment[0][i].add(bQueenLabel);
+
+            } else if (i == 4) {
+                JLabel wKingLabel = new JLabel(UnicodeMap.wKing);
+                initializePiece(wKingLabel);
+                boardSegment[7][i].add(wKingLabel);
+
+                JLabel bKingLabel = new JLabel(UnicodeMap.bKing);
+                initializePiece(bKingLabel);
+                boardSegment[0][i].add(bKingLabel);
+            }
+        }
+    }
+    
+}
