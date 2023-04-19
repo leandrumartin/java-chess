118f0d86f3ecb87702054e22f8b2696ecd7135c4
Leandru Martin <leandru.martin@slu.edu>
2023-04-18 19:44:04 -0500

    2-player controller handles piece and destination selection and enabling/disabling of buttons on new player turn

--- app/src/main/java/Chess/controller/ControllerInterface.java
+++ app/src/main/java/Chess/controller/ControllerInterface.java
@@ -1,5 +1,23 @@
 package Chess.controller;
 
 public interface ControllerInterface {
+
+    public void start();
+
+    /**
+     * Handles selection of the chesspiece to move
+     * @param row row of selected piece
+     * @param col column of selected piece
+     */
+    public void selectPiece(int row, int col);
+
+    /**
+     * 
+     * @param row
+     * @param col
+     */
+    public void selectDestination(int row, int col);
+
+    public void endGame();
     
 }
